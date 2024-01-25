edd715cdf92d569a53eaae5bd617b8f4d97c7b1d
0x5c4r3 <16732213+0x5c4r3@users.noreply.github.com>
2024-01-25 17:27:38 +0100

    Create copyFunc.js

---
+++ js/copyFunc.js
@@ -0,0 +1,50 @@
+<script type="text/javascript">
+  function InitCopyPaste(){
+    const codeBlocks = document.querySelectorAll("div.highlighter-rouge");
+
+    codeBlocks.forEach((codeblock, index) => {
+      const code = codeBlocks[index].innerText;
+      const copyCodeButton = document.createElement("button");
+      copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>';
+      //copyCodeButton.style.color = '#DBDBDB';
+      //copyCodeButton.style.borderColor = '#252525';
+      //copyCodeButton.classList = "btn btn-sm btn-outline-primary";
+      
+      
+      //mouseover
+      copyCodeButton.addEventListener('mouseover', () => {
+      copyCodeButton.style.borderColor = '#DBDBDB';
+      copyCodeButton.style.color = '#DBDBDB';
+      });
+
+      //mouseout
+      copyCodeButton.addEventListener('mouseout', () => {
+      copyCodeButton.style.borderColor = '#252525';
+      copyCodeButton.style.color = '#DBDBDB';
+      });
+      
+      //onclick
+      copyCodeButton.onclick = function () {
+        window.navigator.clipboard.writeText(code);
+        copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-check" aria-hidden="true"></i>';
+        copyCodeButton.style.borderColor = 'red';
+        copyCodeButton.style.color = '#DBDBDB';
+        //copyCodeButton.classList.add("btn-success");
+        //copyCodeButton.classList.remove("btn-outline-primary");
+
+        setTimeout(() => {
+          copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>';
+          copyCodeButton.classList.remove("btn-success");
+          copyCodeButton.style.backgroundColor = '#252525';
+          copyCodeButton.style.borderColor = '#252525';
+          copyCodeButton.style.color = '#DBDBDB';
+          copyCodeButton.classList.add("btn-outline-primary");
+        }, 500);
+      };
+      // make the button
+      codeblock.appendChild(copyCodeButton);
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", InitCopyPaste);
+</script>
