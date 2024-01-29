d4ef1a4d63f70e4bbce2749e4cea67f466968e4c
0x5c4r3 <16732213+0x5c4r3@users.noreply.github.com>
2024-01-29 10:40:22 +0100

    Update copyFunc.js

--- js/copyFunc.js
+++ js/copyFunc.js
@@ -1,48 +1,47 @@
- function InitCopyPaste(){
-    const codeBlocks = document.querySelectorAll("div.highlighter-rouge");
+function InitCopyPaste () {
+  const codeBlocks = document.querySelectorAll('div.highlighter-rouge')
 
-    codeBlocks.forEach((codeblock, index) => {
-      const code = codeBlocks[index].innerText;
-      const copyCodeButton = document.createElement("button");
-      copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>';
-      //copyCodeButton.style.color = '#DBDBDB';
-      //copyCodeButton.style.borderColor = '#252525';
-      //copyCodeButton.classList = "btn btn-sm btn-outline-primary";
-      
-      
-      //mouseover
-      copyCodeButton.addEventListener('mouseover', () => {
-      copyCodeButton.style.borderColor = '#DBDBDB';
-      copyCodeButton.style.color = '#DBDBDB';
-      });
+  codeBlocks.forEach((codeblock, index) => {
+    const code = codeBlocks[index].innerText
+    const copyCodeButton = document.createElement('button')
+    copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>'
+    // copyCodeButton.style.color = '#DBDBDB';
+    // copyCodeButton.style.borderColor = '#252525';
+    // copyCodeButton.classList = "btn btn-sm btn-outline-primary";
 
-      //mouseout
-      copyCodeButton.addEventListener('mouseout', () => {
-      copyCodeButton.style.borderColor = '#252525';
-      copyCodeButton.style.color = '#DBDBDB';
-      });
-      
-      //onclick
-      copyCodeButton.onclick = function () {
-        window.navigator.clipboard.writeText(code);
-        copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-check" aria-hidden="true"></i>';
-        copyCodeButton.style.borderColor = 'red';
-        copyCodeButton.style.color = '#DBDBDB';
-        //copyCodeButton.classList.add("btn-success");
-        //copyCodeButton.classList.remove("btn-outline-primary");
+    // mouseover
+    copyCodeButton.addEventListener('mouseover', () => {
+      copyCodeButton.style.borderColor = '#DBDBDB'
+      copyCodeButton.style.color = '#DBDBDB'
+    })
 
-        setTimeout(() => {
-          copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>';
-          //copyCodeButton.classList.remove("btn-success");
-          copyCodeButton.style.backgroundColor = '#252525';
-          copyCodeButton.style.borderColor = '#252525';
-          copyCodeButton.style.color = '#DBDBDB';
-          //copyCodeButton.classList.add("btn-outline-primary");
-        }, 500);
-      };
-      // make the button
-      codeblock.appendChild(copyCodeButton);
-    });
-  }
+    // mouseout
+    copyCodeButton.addEventListener('mouseout', () => {
+      copyCodeButton.style.borderColor = '#252525'
+      copyCodeButton.style.color = '#DBDBDB'
+    })
 
-  document.addEventListener("DOMContentLoaded", InitCopyPaste);
+    // onclick
+    copyCodeButton.onclick = function () {
+      window.navigator.clipboard.writeText(code)
+      copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-check" aria-hidden="true"></i>'
+      copyCodeButton.style.borderColor = 'red'
+      copyCodeButton.style.color = '#DBDBDB'
+      // copyCodeButton.classList.add("btn-success");
+      // copyCodeButton.classList.remove("btn-outline-primary");
+
+      setTimeout(() => {
+        copyCodeButton.innerHTML = '<i style="pointer-events:none" class="fa fa-clone" aria-hidden="true"></i>'
+        // copyCodeButton.classList.remove("btn-success");
+        copyCodeButton.style.backgroundColor = '#252525'
+        copyCodeButton.style.borderColor = '#252525'
+        copyCodeButton.style.color = '#DBDBDB'
+        // copyCodeButton.classList.add("btn-outline-primary");
+      }, 500)
+    }
+    // make the button
+    codeblock.appendChild(copyCodeButton)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', InitCopyPaste)
